19778b5fb98971aebc49238f8e062b9769c538b8
Pavel Rychkov <rychkovpavel@yandex.kz>
2021-09-07 01:42:04 +0600

    crated a sting truncation function

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,10 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str.length > maxlength) {
+    let newStr = str.substr(0, maxlength);
+    newStr = newStr.slice(0, -1) + '…';
+    
+    return newStr;
+  }
+
+  return str;
 }
