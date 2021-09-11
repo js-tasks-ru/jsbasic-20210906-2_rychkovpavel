4974c52d49cb72c7b2ce3f4e97cd440628b7e6a9
Pavel Rychkov <rychkovpavel@yandex.kz>
2021-09-11 12:01:56 +0600

    Исправил задачу Усечение строки

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,10 +1,7 @@
 function truncate(str, maxlength) {
   if (str.length > maxlength) {
-    let newStr = str.substr(0, maxlength);
-    newStr = newStr.slice(0, -1) + '…';
-    
-    return newStr;
+    return str.slice(0, maxlength - 1) + '…';
   }
-
+  
   return str;
 }
