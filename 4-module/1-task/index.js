6ea891f96ac92dc141fee37960a1bbcbd39854b7
Pavel Rychkov <rychkovpavel@yandex.kz>
2021-09-28 07:57:09 +0600

    4 module completed

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,13 @@
-function makeFriendsList(friends) {
-  // ваш код...
+function makeFriendsList(arr) {
+  const names = arr.map(item => `<li>${item.firstName} ${item.lastName}</li>`);
+  let liItems = ''; 
+    
+  for (let i = 0; i < names.length; i++) {
+    liItems += names[i];
+  }
+
+  let ul = document.createElement('ul');
+  ul.insertAdjacentHTML('afterBegin', liItems);
+
+  return ul;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,47 @@
 function highlight(table) {
-  // ваш код...
+  let rows = table.rows;
+
+  for (let i = 0; i < rows.length; i++) {
+
+    for (let j = 0; j < rows[i].cells.length; j++) {
+
+      if (rows[i].cells[j].dataset.available === 'true') {
+        rows[i].classList.toggle('available');
+      } else if (rows[i].cells[j].dataset.available === 'false') {
+        rows[i].classList.toggle('unavailable');
+      }   
+    
+    }
+	
+  }
+
+  for (let i = 1; i < rows.length; i++) {
+    if (!(rows[i].classList.contains("available")) && !(rows[i].classList.contains("unavailable"))) {
+      rows[i].setAttribute('hidden', true);
+    }
+  }
+
+  for (let i = 0; i < rows.length; i++) {
+
+    for (let j = 0; j < rows[i].cells.length; j++) {
+
+      if (rows[i].cells[j].innerHTML === 'm') {
+        rows[i].classList.toggle('male');
+      } else if (rows[i].cells[j].innerHTML === 'f') {
+        rows[i].classList.toggle('female');
+      }   
+    }
+
+  }
+
+  for (let i = 0; i < rows.length; i++) {
+
+    for (let j = 0; j < rows[i].cells.length; j++) {
+
+      if (rows[i].cells[j].innerHTML < '18') {
+        rows[i].style.textDecoration = 'line-through';
+      }  
+    }
+
+  }
 }
