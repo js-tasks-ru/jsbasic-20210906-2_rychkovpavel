fd5406b89c98e78369b5d7222eb647b69695dbf2
Pavel Rychkov <rychkovpavel@yandex.kz>
2021-09-28 19:15:30 +0600

    Улучшил код 4 модуль 3 задание

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -9,39 +9,23 @@ function highlight(table) {
         rows[i].classList.toggle('available');
       } else if (rows[i].cells[j].dataset.available === 'false') {
         rows[i].classList.toggle('unavailable');
-      }   
-    
+      }
+          
+      if (rows[i].cells[j].innerHTML === 'm') {
+        rows[i].classList.toggle('male');
+      } else if (rows[i].cells[j].innerHTML === 'f') {
+        rows[i].classList.toggle('female');
+      }
+
+      if (rows[i].cells[j].innerHTML < '18') {
+        rows[i].style.textDecoration = 'line-through';
+      }
     }
-	
   }
 
   for (let i = 1; i < rows.length; i++) {
     if (!(rows[i].classList.contains("available")) && !(rows[i].classList.contains("unavailable"))) {
       rows[i].setAttribute('hidden', true);
     }
-  }
-
-  for (let i = 0; i < rows.length; i++) {
-
-    for (let j = 0; j < rows[i].cells.length; j++) {
-
-      if (rows[i].cells[j].innerHTML === 'm') {
-        rows[i].classList.toggle('male');
-      } else if (rows[i].cells[j].innerHTML === 'f') {
-        rows[i].classList.toggle('female');
-      }   
-    }
-
-  }
-
-  for (let i = 0; i < rows.length; i++) {
-
-    for (let j = 0; j < rows[i].cells.length; j++) {
-
-      if (rows[i].cells[j].innerHTML < '18') {
-        rows[i].style.textDecoration = 'line-through';
-      }  
-    }
-
   }
 }
