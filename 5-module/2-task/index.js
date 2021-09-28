d9fff23f0f3cc59444d6821a8ac6fdbcfd5323f8
Pavel Rychkov <rychkovpavel@yandex.kz>
2021-09-28 18:42:29 +0600

    created function that hide text

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector('.toggle-text-button');
+  let text = document.querySelector('#text');
+
+  button.addEventListener('click', () => {
+    if (!(text.hasAttribute('hidden'))) {
+      text.setAttribute('hidden', true);
+    } else if (text.hasAttribute('hidden')) {
+      text.removeAttribute('hidden');
+    }
+  });
 }
