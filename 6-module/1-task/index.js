0aae25e56e23c59d46cf7858e1e8ef6838bd50a2
Pavel Rychkov <rychkovpavel@yandex.kz>
2021-10-04 16:20:54 +0600

    created class UserTable

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,63 @@
  *
  */
 export default class UserTable {
+  #rows = null;
+  #rowsTemplate = '';
+  #template = '';
+
   constructor(rows) {
+    this.#rows = rows;
+    this.elem = document.createElement('table');
+    this.#rowsTemplate = this.#makeRows(this.#rows);
+    this.#template = this.#makeTemplate();
+    this.render = this.render();
+    this.#onClick = this.#onClick();
+  }
+
+  #makeRows() {
+    return this.#rows.map(item => {
+      return `
+        <tr>
+          <td>${item.name}</td>
+          <td>${item.age}</td>
+          <td>${item.salary}</td>
+          <td>${item.city}</td>
+          <td><button>X</button></td>
+        </tr>
+      `;
+    }).join('');
+  }
+
+  #makeTemplate() {
+    return `
+    <thead>
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      ${this.#rowsTemplate}
+    </tbody>
+  `;
+  }
+
+  render() {
+    this.elem.insertAdjacentHTML('afterBegin', this.#template);
+  }
+
+  get buttons() {
+    return this.elem.querySelectorAll('BUTTON');
+  }
+
+  #onClick = () => {  
+    this.buttons.forEach(item => {
+      item.addEventListener('click', () => {
+        item.parentElement.parentElement.remove();
+      });
+    });
   }
 }
