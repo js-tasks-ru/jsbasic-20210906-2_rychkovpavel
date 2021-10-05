4a615c2b17ca9c47d0c9a5e5b631485cb52f8ac5
Pavel Rychkov <rychkovpavel@yandex.kz>
2021-10-05 21:46:17 +0600

    переделал 6 модуль 2 задание

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,22 +1,25 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class ProductCard {
+  #product = null;
+  #template = '';
+  
   constructor(product) {
-    this.product = product;
-    this.template = this.makeTemplate(this.product);
-    this._container = createElement(this.template);
-    this.onClick();
+    this.#product = product;
+    this.#template = this.makeTemplate(this.#product);
+    this._container = createElement(this.#template);
+    this.button.addEventListener('click', this.onClick);
   }
 
   makeTemplate() {
     return `
       <div class="card">
         <div class="card__top">
-          <img src="/assets/images/products/${this.product.image}" class="card__image" alt="product">
-          <span class="card__price">€${this.product.price.toFixed(2)}</span>
+          <img src="/assets/images/products/${this.#product.image}" class="card__image" alt="product">
+          <span class="card__price">€${this.#product.price.toFixed(2)}</span>
         </div>
         <div class="card__body">
-          <div class="card__title">${this.product.name}</div>
+          <div class="card__title">${this.#product.name}</div>
           <button type="button" class="card__button">
             <img src="/assets/images/icons/plus-icon.svg" alt="icon">
           </button>
@@ -29,20 +32,20 @@ export default class ProductCard {
     return this._container;
   }
 
-  get buttons() {
-    return this._container.querySelectorAll('.card__button');
+  get button() {
+    return this._container.querySelector('.card__button');
+  }  
+  
+  onClick = () => {
+    const productAdd = new CustomEvent('product-add', {
+      detail: this.#product.id,
+      bubbles: true
+    });
+
+    this._container.dispatchEvent(productAdd);
   }
 
-  onClick = () => {
-    this.buttons.forEach(item => {
-      item.addEventListener('click', () => {
-        const productAdd = new CustomEvent('product-add', {
-          detail: this.product.id,
-          bubbles: true
-        });
-    
-        this._container.dispatchEvent(productAdd);
-      });
-    });
+  destroy() {
+    this.button.removeEventListener('click', this.onClick);
   }
 }
